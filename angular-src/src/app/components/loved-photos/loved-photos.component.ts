c1a267c461ab2baea3f94d2f5fdb7f84881bde44
Anh <vongmaianh170196@gmail.com>
2021-06-30 19:22:01 +0300

    last commit

---
+++ angular-src/src/app/components/loved-photos/loved-photos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loved-photos',
+  templateUrl: './loved-photos.component.html',
+  styleUrls: ['./loved-photos.component.css']
+})
+export class LovedPhotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angular-src/src/app/components/my-photos/my-photos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-photos',
+  templateUrl: './my-photos.component.html',
+  styleUrls: ['./my-photos.component.css']
+})
+export class MyPhotosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
